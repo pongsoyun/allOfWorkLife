480930da9c491af828252436cb332b41b3e1941e
pongsoyun <thdbstjdud@gmail.com>
2020-07-22 23:45:55 +0900

    :sparkles: db에 있는 데이터 화면에 보여지게끔 구현 - <div> -> <li> tag로 변경 - db 저장 형태에 "menu" attribute 추가

--- src/index.js
+++ src/index.js
@@ -1,22 +1,75 @@
-// let allDB = JSON.parse(localStorage.getItem('all'));
+const memos = document.querySelector('.memo__list');
+
 let workDB = JSON.parse(localStorage.getItem('work')) || [];
 let lifeDB = JSON.parse(localStorage.getItem('life')) || [];
 let hobbyDB = JSON.parse(localStorage.getItem('hobby')) || [];
 let wishDB = JSON.parse(localStorage.getItem('wish')) || [];
 let healthDB = JSON.parse(localStorage.getItem('health')) || [];
 let habitDB = JSON.parse(localStorage.getItem('habit')) || [];
-
-console.log(wishDB);
+let allDB = [...workDB, ...lifeDB, ...hobbyDB, ...wishDB, ...healthDB, ...habitDB];
+// * 데이터에 아예 menu를 같이 저장하는 편이 db 수정할때도 더 편할 것 같음
+// let allDB = {
+// 	work: workDB,
+// 	life: lifeDB,
+// 	hobby: hobbyDB,
+// 	wish: wishDB,
+// 	health: healthDB,
+// 	habit: habitDB,
+// };
 
 // * submitBtn 눌렸을 때
 const submitBtn = document.querySelector('.input__memo__submit');
 submitBtn.addEventListener('click', (event) => {
 	const menu = getMenu();
 	const text = getText();
-	addMemo(menu, text.value);
+	const id = createUUID();
+
+	addMemo(menu, text.value, id);
+	const memo = getMemoForm(menu, text.value, id);
 
 	text.value = '';
 });
+
+// * 처음 시작했을 때
+allDB.forEach((memo) => {
+	const form = getMemoForm(memo.menu, memo.text, memo.id);
+	// console.log(form);
+	memos.appendChild(form);
+});
+
+// * 메모 폼 하나 만들어주기
+function getMemoForm(menu, text, id) {
+	const memo = document.createElement('li');
+	memo.setAttribute('class', 'memo');
+	memo.setAttribute('id', id);
+	memo.innerHTML = `
+    <div class="memo__text">
+	    ${text}						
+	</div>
+    <div class="memo__info">
+		<span class="memo__menu">${menu}</span>
+		<i class="fas fa-trash-alt memo__delete__btn"></i>
+	</div>
+    `;
+
+	memo.addEventListener('click', (event) => {
+		if (event.target.className === 'memo__text') {
+			// 수정하기
+			console.log('edit button pressed');
+		}
+
+		if (event.target.clasName === 'memo__menu') {
+			// menu 수정하기? -> 보류
+		}
+
+		if (event.target.className === 'memo__delete__btn') {
+			// 삭제하기
+			console.log('delete button pressed');
+		}
+	});
+
+	return memo;
+}
 
 // * UUID
 function createUUID() {
@@ -24,7 +77,7 @@ function createUUID() {
 }
 
 // * 수정시에도 재사용할수있도록
-function addMemo(menu, text) {
+function addMemo(menu, text, id) {
 	console.log(`가져온 menu: ${menu}, text: ${text}`);
 
 	// main 화면 Update
@@ -53,8 +106,7 @@ function addMemo(menu, text) {
 			break;
 	}
 
-	const id = createUUID();
-	localStorage.setItem(menu, JSON.stringify([...db, { text, id }])); // id, star ?
+	localStorage.setItem(menu, JSON.stringify([...db, { menu, text, id }])); // id, star ?
 	db = JSON.parse(localStorage.getItem(menu));
 
 	switch (menu) {
